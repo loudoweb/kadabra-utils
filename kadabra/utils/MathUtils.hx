59ce84ceae71ff55eec3496476c45a9c10b6e31c
Ludovic Bas <lugludum@gmail.com>
2022-12-14 14:11:43 +0100

    add method to get point along a line at certain radius

--- kadabra/utils/MathUtils.hx
+++ kadabra/utils/MathUtils.hx
@@ -18,4 +18,20 @@ class MathUtils {
 		var dy:Float = pt1.y - pt2.y;
 		return Math.sqrt(dx * dx + dy * dy);
 	}
+
+	inline static public function normalize(pt1:Point):Point {
+		var l = pt1.length;
+		return new Point(pt1.x / l, pt1.y / l);
+	}
+
+	inline static public function getPointAtDistance(pt1:Point, pt2:Point, radius:Float, ?pout:Point):Point {
+		var vector:Point = new Point(pt2.x - pt1.x, pt2.y - pt1.y);
+		vector = normalize(vector);
+		if (pout == null)
+			return new Point(pt1.x + radius * vector.x, pt1.y + radius * vector.y);
+		else{
+			pout.setTo(pt1.x + radius * vector.x, pt1.y + radius * vector.y);
+			return pout;
+		}
+	}
 }
